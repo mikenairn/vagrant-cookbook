527141db54415dc32b7c0a0bc5cf42653d83fcd7
Michael Nairn <michael.nairn@feedhenry.com>
2013-04-11 12:11:38 +0100

    license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'vagrant'
 maintainer       'FeedHenry'
 maintainer_email 'michael.nairn@feedhenry.com'
-license          'All rights reserved'
+license           'Apache 2.0'
 description      'Installs/Configures vagrant'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
