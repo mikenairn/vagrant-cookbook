f6f512454ccb634387c590a0f318153e6fe9cfec
Michael Nairn <michael.nairn@feedhenry.com>
2013-05-24 16:25:35 +0100

    Set correct filename for versions > 1.2 Bump default version to 1.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,22 +1,31 @@
 default['vagrant']['download_url'] = 'http://files.vagrantup.com/packages'
-default['vagrant']['version'] = '1.1.4'
-default['vagrant']['version_hash'] = '87613ec9392d4660ffcb1d5755307136c06af08c'
+default['vagrant']['version'] = '1.2.2'
+default['vagrant']['version_hash'] = '7e400d00a3c5a0fdf2809c8b5001a035415a607b'
 default['vagrant']['arch'] = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i686"
 
 case node['platform_family']
+  when 'mac_os_x'
+    file_name = vagrant['version'].to_f >= 1.2 ? "Vagrant-#{vagrant['version']}" : 'Vagrant'
+  when 'windows'
+    file_name = vagrant['version'].to_f >= 1.2 ? "Vagrant_#{vagrant['version']}" : 'Vagrant'
+  else
+    file_name = vagrant['version'].to_f >= 1.2 ? "vagrant_#{vagrant['version']}_#{vagrant['arch']}" : "vagrant_#{vagrant['arch']}"
+end
+
+case node['platform_family']
   when 'redhat', 'fedora'
-    default['vagrant']['rpm'] = "vagrant_#{vagrant['arch']}.rpm"
+    default['vagrant']['rpm'] = "#{file_name}.rpm"
     default['vagrant']['rpm_url'] = "#{node['vagrant']['download_url']}/#{node['vagrant']['version_hash']}/#{node['vagrant']['rpm']}"
   when 'windows'
-    default['vagrant']['msi'] = 'Vagrant.msi'
+    default['vagrant']['msi'] = "#{file_name}.msi"
     default['vagrant']['msi_url'] = "#{node['vagrant']['download_url']}/#{node['vagrant']['version_hash']}/#{node['vagrant']['msi']}"
   when 'mac_os_x'
-    default['vagrant']['dmg'] = 'Vagrant.dmg'
+    default['vagrant']['dmg'] = "#{file_name}.dmg"
     default['vagrant']['dmg_url'] = "#{node['vagrant']['download_url']}/#{node['vagrant']['version_hash']}/#{node['vagrant']['dmg']}"
   when 'debian', 'ubuntu'
-    default['vagrant']['deb'] = "vagrant_#{vagrant['arch']}.deb"
+    default['vagrant']['deb'] = "#{file_name}.deb"
     default['vagrant']['deb_url'] = "#{node['vagrant']['download_url']}/#{node['vagrant']['version_hash']}/#{node['vagrant']['deb']}"
   else
-    default['vagrant']['tar'] = "vagrant_#{vagrant['arch']}.pkg.tar.xz"
+    default['vagrant']['tar'] = "#{file_name}.pkg.tar.xz"
     default['vagrant']['tar_url'] = "#{node['vagrant']['download_url']}/#{node['vagrant']['version_hash']}/#{node['vagrant']['tar']}"
 end
